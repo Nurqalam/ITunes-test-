03315b9e56dbad87b7b336462cfb022ddefdc7e0
Nurqalam <tnurkalam@gmail.com>
2022-05-18 15:00:31 +0500

    AlbumsViewController + cell

--- ITunesProjectTest/Controllers/AlbumsViewController.swift
+++ ITunesProjectTest/Controllers/AlbumsViewController.swift
@@ -11,10 +11,97 @@ import UIKit
 
 class AlbumsViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let tablView = UITableView()
+        tablView.register(AlbumsViewControllerCell.self, forCellReuseIdentifier: "cell")
+        tablView.translatesAutoresizingMaskIntoConstraints = false
+        return tablView
+    }()
+    
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        setupViews()
+        setDelegates()
+        setConstraints()
+        setNavigationBar()
+        setupSearchController()
     }
     
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+        title = "Albums"
+        
+    }
+    
+    private func setDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setNavigationBar() {
+        title = "Albums"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        self.navigationController?.navigationBar.tintColor = .black
+        navigationItem.searchController = searchController
+        
+        let userInfoButton = createCustomButton(selector: #selector(userInfoButtonTapped))
+        navigationItem.rightBarButtonItem = userInfoButton
+    }
+    
+    private func setupSearchController() {
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    @objc private func userInfoButtonTapped() {
+//        let userInfoViewController = UserInfoViewController()
+//        navigationController?.pushViewController(userInfoViewController, animated: true)
+        print("hello")
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+extension AlbumsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        70
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+extension AlbumsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? AlbumsViewControllerCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    
+}
+
+
+// MARK: - setConstraints
+extension AlbumsViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
